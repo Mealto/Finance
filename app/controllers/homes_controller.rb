bc49d4ee33d4033c8d6a21dfc9612999c9507b97
Ankush Sharma <kushsharma057@gmail.com>
2023-10-31 17:36:12 +0530

    [WIP] Added home page basic design and button

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+  def home_page
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "homes#home_page"
+  get 'contact_us/new', to: 'contact_us#new'
+  post 'submit_contact_us', to: 'contact_us#submit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
